d21708d5ae71f856a27478fc1b509e6bcb58ba2b
OmPrakash Kuswaha <okuswaha@routeone.com>
2017-01-09 10:27:37 -0500

    added needed dependencies in pom, added a configuration and aplication classes

---
+++ src/main/java/com/prakash/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.prakash.config;
+
+/**
+ * Created by okuswaha on 1/9/2017.
+ */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/css/**", "/index").permitAll()
+                .antMatchers("/user/**").hasRole("USER")
+                .and()
+                .formLogin();
+
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }
+}
